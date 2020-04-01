4093a137934eda3ed135e30b075548158abb7e8f
Shivam Balwani <shivambalwani0407@gmail.com>
2020-04-01 05:54:22 +0530

    Navigation Bar added

--- music_player/urls.py
+++ music_player/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('music/',include('music.urls')),
+    path('',include('music.urls')),
     path('admin/', admin.site.urls),
 ]
