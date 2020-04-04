d659d23294fcea9cccc002e1c41f4379a99d90ec
Shivam Balwani <shivambalwani0407@gmail.com>
2020-04-04 21:38:34 +0530

    virtualenv added

--- music/models.py
+++ music/models.py
@@ -1,10 +1,15 @@
 from django.db import models
-  
+from django.urls import reverse
+
+
 class Album(models.Model):
     artist=models.CharField(max_length=50)
     album_title=models.CharField(max_length=500)
     genre=models.CharField(max_length=50)
     album_logo=models.CharField(max_length=200)
+
+    def get_absolute_url(self):
+        return reverse('music:details',kwargs={"pk":self.pk})
 
     def __str__(self):
         return self.artist+'=>'+self.album_title
